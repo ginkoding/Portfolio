e5f08971ccb4aeb95d1e40d37230573369c4bcaf
Rebecca Sewell <rebecca.lee.sewell@gmail.com>
2022-02-25 09:28:38 +0000

    leaf icon dropdwn

--- app.js
+++ app.js
@@ -9,15 +9,3 @@ function myFunction() {
 
 
 
-//   window.onscroll = function() {myFunction()};
-
-//   let navbar = document.getElementById("myTopnav");
-//   let sticky = navbar.offsetTop;
-  
-//   function myFunction() {
-//     if (window.pageYOffset >= sticky) {
-//       navbar.classList.add("sticky")
-//     } else {
-//       navbar.classList.remove("sticky");
-//     }
-//   }
